84987a4adcb83df54788f38679483502d900e387
jiaren <jiaaren@github.com>
2021-05-18 11:03:28 +0800

    added bonus files

---
+++ get_next_line_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 21:11:17 by jkhong            #+#    #+#             */
+/*   Updated: 2021/05/18 11:02:42 by jkhong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	check_newline(char *str, char *buffer)
+{	
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			while (str[i + j + 1])
+			{
+				buffer[j] = str[i + j + 1];
+				j++;
+			}
+			buffer[j] = '\0';
+			return (i);
+		}
+		i++;
+	}
+	buffer[j] = '\0';
+	return (-1);
+}
+
+char	*concat_lst(t_list *lst, int len)
+{
+	char	*tmp;
+	int		i_tmp;
+	int		i_lst;
+
+	tmp = malloc(sizeof(char) * (len + 1));
+	if (!tmp)
+		return (NULL);
+	i_tmp = 0;
+	while (lst)
+	{
+		i_lst = 0;
+		while ((lst->content)[i_lst] && (lst->content)[i_lst] != '\n')
+			tmp[i_tmp++] = (lst->content)[i_lst++];
+		lst = lst->next;
+	}
+	tmp[i_tmp] = '\0';
+	return (tmp);
+}
+
+int	add_str(int fd, char *buffer, t_list **lst, char **str)
+{
+	int		index;
+	char	*char_tmp;
+
+	index = 0;
+	char_tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buffer[0])
+	{	
+		while (buffer[index])
+		{
+			char_tmp[index] = buffer[index];
+			index++;
+		}
+	}
+	else
+		index = read(fd, char_tmp, BUFFER_SIZE);
+	if (index == -1)
+	{	
+		free(char_tmp);
+		return (-1);
+	}
+	char_tmp[index] = '\0';
+	*str = char_tmp;
+	ft_lstadd_back(lst, char_tmp);
+	return (index);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	int			index;
+	char		*char_tmp;
+	t_list		*lst;
+
+	index = 1;
+	char_tmp = NULL;
+	lst = NULL;
+	while (index)
+	{
+		index = add_str(fd, buff, &lst, &char_tmp);
+		if (index == -1)
+			return (-1);
+		else if (check_newline(char_tmp, buff) >= 0 || index == 0)
+		{
+			*line = concat_lst(lst, ft_linesize(lst));
+			ft_lstclear(&lst, free);
+			if (!index)
+				return (0);
+			return (1);
+		}
+	}
+	return (0);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 21:17:55 by jkhong            #+#    #+#             */
+/*   Updated: 2021/05/18 11:02:44 by jkhong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 40
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}					t_list;
+
+int		get_next_line(int fd, char **line);
+void	ft_lstadd_back(t_list **lst, char *content);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+int		ft_lstsize(t_list *lst);
+int		ft_linesize(t_list *lst);
+char	*ft_strdup(const char *str);
+void	print_lst(t_list *lst);
+
+#endif
