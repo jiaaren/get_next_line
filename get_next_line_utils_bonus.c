05a2e40cf86d69a1ab90ccebc2f154a3266e276a
Jia ren Khong <jkhong@u90z04s03.42kl.edu.my>
2021-05-22 12:20:37 +0800

    changes to (i) update header reference to bonus .h files and (ii) consideration of limits of FD size

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 void	ft_lstadd_back(t_list **lst, char *content)
 {
